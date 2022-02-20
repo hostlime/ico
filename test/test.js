79d94073894166a9dc121a4887e1ddda2d9228d2
hostlime <hostlime@ya.ru>
2022-02-21 00:19:09 +0300

    Удалил тестовый код из тестов

--- test/test.js
+++ test/test.js
@@ -1,16 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect, assert } = require("chai");
 
-describe("MyCryptoLions", function () {
-  it("Should return the right name and symbol", async function () {
-    const MyCryptoLions = await hre.ethers.getContractFactory("MyCryptoLions");
-    const myCryptoLions = await MyCryptoLions.deploy("MyCryptoLions", "MCL");
-
-    await myCryptoLions.deployed();
-    expect(await myCryptoLions.name()).to.equal("MyCryptoLions");
-    expect(await myCryptoLions.symbol()).to.equal("MCL");
-  });
-});
 
 describe.only("Checking ICO contract", function () {
     let ico;
